1566bd9badf65ba94fcc4c5d54b0c5cb67198f50
terry <976912095@qq.com>
2020-06-13 15:06:24 +0800

    'ant'

--- React/react-demo/src/App.js
+++ React/react-demo/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import ListItem from './components/listItem'
+import {Route} from 'react-router-dom'
 
 function App() {
   return (
@@ -10,4 +11,17 @@ function App() {
   );
 }
 
+class App extends Component {
+  render() { 
+    return ( 
+      <div>
+        <Switch>
+          <Route path='/aa' component='aa' />
+          <Route path='/' component='Home' />
+        </Switch>
+      </div>
+     );
+  }
+}
+ 
 export default App;
