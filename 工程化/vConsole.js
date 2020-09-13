f4cfc8d0b4463b6defa6b55f372e9b6f5bcc11d6
terry <976912095@qq.com>
2020-09-13 16:08:26 +0800

    'promise'

---
+++ 工程化/vConsole.js
@@ -0,0 +1,30 @@
+// 1. import vConsole
+const VConsole = require('path/to/vconsole.min.js')
+const vConsole = new VConsole()
+
+
+// 2. Create plugin object  
+const myPlugin = vConsole.VConsolePlugin('my_plugin', 'My Plugin')
+
+// 3. Bind plugin events
+myPlugin.on('init', () => {
+    console.log('My Plugin init')
+})
+
+myPlugin.on('renderTab', (callback) => {
+    const html = `<div>Hello VConsole!</div>`
+    callback(html)
+})
+
+myPlugin.on('addTool', (callback) => {
+    let button = {
+        name: 'Reload',
+        onCLick: function(event) {
+            location.reload()
+        }
+    }
+    callback([button])
+})
+
+// 4. Add to vConsole
+vConsole.addPlugin(myPlugin)
